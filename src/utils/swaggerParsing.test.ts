a8ed5455aec7ef5d355729d651cc9b21f6d9e4e9
Marco LUCARELLA <marco.lucarella@amadeus.com>
2023-01-24 20:53:54 +0100

    Test setup for swaggerParsing

---
+++ src/utils/swaggerParsing.test.ts
@@ -0,0 +1,11 @@
+describe('SwaggerParsing', () => {
+    // describe('resolveReferences', () => {
+    //     test('Not an object', () => {
+    //         const kInput = { a: 1, b: 2 }
+    //         const kExpected = kInput
+    //         expect(resolveReferences(kInput)).toEqual(kExpected)
+    //     })
+    // })
+})
+
+export { }
