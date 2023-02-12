60f83b323c731ed8adb700916e5cb6dbd120be10
Marco LUCARELLA <lucarellamarco@yahoo.it>
2023-02-12 23:36:35 +0100

    Generation remove useless files

--- generate.mjs
+++ generate.mjs
@@ -9,37 +9,15 @@ const INPUT_FOLDER = './inputApi'
 const OUTPUT_FOLDER = './public/apis'
 const INDEX_FILE_PATH = './src/apiIndex.json'
 const API_SUFFIX = '.api.json'
-const DIAGRAMS_SUFFIX = '.diagrams.json'
-const TABLES_SUFFIX = '.tables.json'
 const VALIDATION_SUFFIX = '.validation.json'
-const RELEASE_NOTES_SUFFIX = '.release-notes.json'
-
-const DIAGRAM_BUILDER_OPTIONS = {
-}
-
 
 async function generateApi(apiDoc, apiHash) {
     await fs.outputJson(`${OUTPUT_FOLDER}/${apiHash}${API_SUFFIX}`, apiDoc)
 }
 
-async function generateDiagrams(apiDoc, apiHash) {
-    const diagrams = await apiTools.apiToPlantUmlDiagrams(apiDoc, DIAGRAM_BUILDER_OPTIONS)
-    await fs.outputJson(`${OUTPUT_FOLDER}/${apiHash}${DIAGRAMS_SUFFIX}`, diagrams)
-}
-
-async function generateTables(apiDoc, apiHash) {
-    const tables = await apiTools.apiToTables(apiDoc)
-    await fs.outputJson(`${OUTPUT_FOLDER}/${apiHash}${TABLES_SUFFIX}`, tables)
-}
-
 async function generateValidation(apiDoc, apiHash) {
     await fs.outputJson(`${OUTPUT_FOLDER}/${apiHash}${VALIDATION_SUFFIX}`, {})
 }
-
-async function generateReleaseNotes(apiDoc, apiHash) {
-    await fs.outputJson(`${OUTPUT_FOLDER}/${apiHash}${RELEASE_NOTES_SUFFIX}`, {})
-}
-
 
 glob(`${INPUT_FOLDER}/**/*.+(json|yaml|yml)`, async (error, fileNames) => {
     if (error) {
@@ -53,10 +31,7 @@ glob(`${INPUT_FOLDER}/**/*.+(json|yaml|yml)`, async (error, fileNames) => {
         const apiHash = hash(apiDoc)
 
         await generateApi(apiDoc, apiHash)
-        await generateDiagrams(apiDoc, apiHash)
-        await generateTables(apiDoc, apiHash)
         await generateValidation(apiDoc, apiHash)
-        await generateReleaseNotes(apiDoc, apiHash)
 
         const api = await apiTools.parseApi(apiDoc)
 
@@ -67,5 +42,4 @@ glob(`${INPUT_FOLDER}/**/*.+(json|yaml|yml)`, async (error, fileNames) => {
         })
     }
     fs.outputJson(INDEX_FILE_PATH, apiIndex)
-
 })
