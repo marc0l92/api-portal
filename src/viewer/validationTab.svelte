f22dacf93e3e81bc3b0809f1a5f8f16c8ed20abe
mlucarella <Marco.LUCARELLA@amadeus.com>
2023-03-23 10:30:29 +0100

    Limit items in the validation tab

--- src/viewer/validationTab.svelte
+++ src/viewer/validationTab.svelte
@@ -1,21 +1,32 @@
 <script lang="ts">
     import { getValidationSummary, severityNames, type ApiValidation, type ValidationSummary } from './validation';
 
+    const INITIAL_LIMIT = 5;
+    const LIMIT_INCREMENT = 3;
+
     export let validationData: ApiValidation[] = [];
     export let validationSummary: ValidationSummary = {};
     let selectedErrorCode: string = '';
+    let validationItemsLimit = INITIAL_LIMIT;
+    let validationDataFiltered: ApiValidation[] = [];
 
     function selectErrorCode(errorCode: string) {
+        validationItemsLimit = INITIAL_LIMIT;
         if (selectedErrorCode === errorCode) {
             selectedErrorCode = '';
         } else {
             selectedErrorCode = errorCode;
         }
     }
+    function loadMoreItems() {
+        validationItemsLimit += LIMIT_INCREMENT;
+    }
 
     $: if (validationData) {
         validationSummary = getValidationSummary(validationData);
     }
+
+    $: validationDataFiltered = validationData ? validationData.filter((item) => !selectedErrorCode || selectedErrorCode === item.code) : [];
 </script>
 
 {#if validationData}
@@ -40,15 +51,16 @@
             </aside>
         </div>
         <div class="column">
-            {#each validationData as validationItem}
-                {#if !selectedErrorCode || selectedErrorCode === validationItem.code}
-                    <div class="notification {severityNames[validationItem.severity].css}">
-                        <span><strong>Code</strong>: {validationItem.code}</span><br />
-                        <span><strong>Message</strong>: {validationItem.message}</span><br />
-                        <span><strong>Path</strong>: /{validationItem.path.join('/')}</span>
-                    </div>
-                {/if}
+            {#each validationDataFiltered.slice(0, validationItemsLimit) as validationItem}
+                <div class="notification {severityNames[validationItem.severity].css}">
+                    <span><strong>Code</strong>: {validationItem.code}</span><br />
+                    <span><strong>Message</strong>: {validationItem.message}</span><br />
+                    <span><strong>Path</strong>: /{validationItem.path.join('/')}</span>
+                </div>
             {/each}
+            {#if validationItemsLimit < validationDataFiltered.length}
+                <button class="button" on:click={loadMoreItems}>Load more items</button>
+            {/if}
         </div>
     </div>
 {/if}
