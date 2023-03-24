bdcfcff839a4914b0c8d333edd564686b46eecf9
mlucarella <Marco.LUCARELLA@amadeus.com>
2023-03-24 16:39:55 +0100

    Compare input boxes

--- src/compare/index.svelte
+++ src/compare/index.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Footer from 'components/footer.svelte';
+  import InputApi from 'components/inputApi.svelte';
   import Navbar from 'components/navbar.svelte';
 </script>
 
@@ -8,11 +9,23 @@
   <section class="hero is-small">
     <div class="hero-body">
       <h1 class="title">Api Compare</h1>
-      <p class="subtitle">Work in progress...</p>
+      <p class="subtitle">List changes between two Apis</p>
     </div>
   </section>
+  <div>
+    <div class="columns">
+      <div class="column"><InputApi /></div>
+      <div class="column is-center is-narrow"><button class="button"><i class="fa-solid fa-right-left" /></button></div>
+      <div class="column"><InputApi /></div>
+    </div>
+  </div>
 </div>
 <Footer />
 
 <style>
+  .is-center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
