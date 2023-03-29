b8ef057c22f400aa61754f64f3a3c11758f7e639
mlucarella <Marco.LUCARELLA@amadeus.com>
2023-03-29 18:49:11 +0200

    Fix compare parameters

--- src/compare/compare.ts
+++ src/compare/compare.ts
@@ -1,4 +1,5 @@
 import type { Api, ApiParameterDoc, ApiParameterDocMap, ApiService } from "common/api/api"
+import type { ApiModelDoc } from "common/api/apiModel"
 
 export interface ApiDiff {
     isBackwardCompatible: boolean
@@ -53,9 +54,10 @@ export function compareApis(leftApi: Api, rightApi: Api) {
                 type: DiffType.MODIFIED,
                 metadata: [], parameters: [], request: [], responses: {},
             }
+            // console.log(leftService.getName())
             serviceDiff.metadata = compareMetadata(extractMetadataFromService(leftService), extractMetadataFromService(rightService))
             serviceDiff.parameters = compareRequestParameters(leftService.getRequestParameters(), rightService.getRequestParameters())
-            serviceDiff.request = compareRequests(leftService.getRequest(), rightService.getRequest())
+            serviceDiff.request = compareApiParameters(leftService.getRequest(), rightService.getRequest(), 'Request body')
             serviceDiff.responses = compareResponses(leftService.getResponses(), rightService.getResponses())
 
             if (serviceDiff.metadata.length || serviceDiff.parameters.length || serviceDiff.request.length || Object.keys(serviceDiff.responses).length) {
@@ -94,6 +96,12 @@ function extractMetadataFromService(service: ApiService) {
     )
 }
 
+function extractMetadataFromApiParameter(service: ApiParameterDoc) {
+    return Object.fromEntries(Object.entries(service)
+        .filter(([key, _]) => ['schema'].indexOf(key) === -1)
+    )
+}
+
 function getPath(basePath: string, key: string, isArray: boolean): string {
     if (isArray) {
         return `${basePath}[${key}]`
@@ -102,41 +110,44 @@ function getPath(basePath: string, key: string, isArray: boolean): string {
     }
 }
 
-function compareMetadata(leftDoc: any, rightDoc: any, path: string = ''): DiffItem[] {
+function compareMetadata(leftDoc: any, rightDoc: any, basePath: string = '', notBackwardCompatiblePaths: string[] = []): DiffItem[] {
     let diffItems: DiffItem[] = []
     const rightKeys = Object.keys(rightDoc)
     for (const leftKey of Object.keys(leftDoc)) {
         const rightKeysIndex = rightKeys.indexOf(leftKey)
         if (rightKeysIndex >= 0) {
             if (typeof leftDoc[leftKey] === 'object' && typeof rightDoc[leftKey] === 'object') {
-                diffItems = diffItems.concat(compareMetadata(leftDoc[leftKey], rightDoc[leftKey], getPath(path, leftKey, Array.isArray(leftDoc))))
+                diffItems = diffItems.concat(compareMetadata(leftDoc[leftKey], rightDoc[leftKey], getPath(basePath, leftKey, Array.isArray(leftDoc)), notBackwardCompatiblePaths))
             } else {
                 if (leftDoc[leftKey] !== rightDoc[leftKey]) {
+                    const path = getPath(basePath, leftKey, Array.isArray(leftDoc))
                     diffItems.push({
-                        path: getPath(path, leftKey, Array.isArray(leftDoc)),
+                        path: path,
                         type: DiffType.MODIFIED,
                         leftValue: leftDoc[leftKey],
                         rightValue: rightDoc[leftKey],
-                        isBackwardCompatible: true,
+                        isBackwardCompatible: notBackwardCompatiblePaths.indexOf(path) === -1,
                     })
                 }
             }
             rightKeys.splice(rightKeysIndex, 1)
         } else {
+            const path = getPath(basePath, leftKey, Array.isArray(leftDoc))
             diffItems.push({
-                path: getPath(path, leftKey, Array.isArray(leftDoc)),
+                path: path,
                 type: DiffType.REMOVED,
                 leftValue: leftDoc[leftKey],
-                isBackwardCompatible: true,
+                isBackwardCompatible: notBackwardCompatiblePaths.indexOf(path) === -1,
             })
         }
     }
     for (const rightKey of rightKeys) {
+        const path = getPath(basePath, rightKey, Array.isArray(rightDoc))
         diffItems.push({
-            path: getPath(path, rightKey, Array.isArray(rightDoc)),
+            path: path,
             type: DiffType.ADDED,
             rightValue: rightDoc[rightKey],
-            isBackwardCompatible: true,
+            isBackwardCompatible: notBackwardCompatiblePaths.indexOf(path) === -1,
         })
     }
     return diffItems
@@ -144,11 +155,13 @@ function compareMetadata(leftDoc: any, rightDoc: any, path: string = ''): DiffIt
 
 
 function compareRequestParameters(leftRequestParameters: ApiParameterDoc[], rightRequestParameters: ApiParameterDoc[]): DiffItem[] {
-    const diffItems: DiffItem[] = []
+    let diffItems: DiffItem[] = []
     const rightRequestParametersIndexes = Object.keys(rightRequestParameters).map(x => parseInt(x))
     for (const leftRequestParameter of leftRequestParameters) {
         const rightRequestParameterIndex = rightRequestParameters.findIndex(p => p.name === leftRequestParameter.name && p.in === leftRequestParameter.in)
         if (rightRequestParameterIndex >= 0) {
+            const rightRequestParameter = rightRequestParameters[rightRequestParameterIndex]
+            diffItems = diffItems.concat(compareApiParameters(leftRequestParameter, rightRequestParameter, `${leftRequestParameter.name} [${leftRequestParameter.in}]`))
             rightRequestParametersIndexes.splice(rightRequestParametersIndexes.indexOf(rightRequestParameterIndex), 1)
         } else {
             diffItems.push({
@@ -171,28 +184,36 @@ function compareRequestParameters(leftRequestParameters: ApiParameterDoc[], righ
     return diffItems
 }
 
-function compareRequests(leftRequest: ApiParameterDoc, rightRequest: ApiParameterDoc): DiffItem[] {
-    const diffItems: DiffItem[] = []
-    if (!leftRequest && rightRequest) {
+function compareApiParameters(leftParameter: ApiParameterDoc, rightParameter: ApiParameterDoc, basePath: string): DiffItem[] {
+    let diffItems: DiffItem[] = []
+    if (!leftParameter && rightParameter) {
         return [{
-            path: `Request body`,
+            path: basePath,
             type: DiffType.ADDED,
-            rightValue: rightRequest,
+            rightValue: rightParameter,
             isBackwardCompatible: true,
         }]
     }
-    if (leftRequest && !rightRequest) {
+    if (leftParameter && !rightParameter) {
         return [{
-            path: `Request body`,
+            path: basePath,
             type: DiffType.REMOVED,
-            rightValue: leftRequest,
+            rightValue: leftParameter,
             isBackwardCompatible: false,
         }]
     }
+    const notBackwardCompatiblePaths: string[] = ['in', 'name', 'type', 'required', 'schema']
+    diffItems = diffItems.concat(compareMetadata(extractMetadataFromApiParameter(leftParameter), extractMetadataFromApiParameter(rightParameter), basePath, notBackwardCompatiblePaths))
+    diffItems = diffItems.concat(compareModels(leftParameter.schema, rightParameter.schema, basePath))
     return diffItems
 }
 
 function compareResponses(leftResponses: ApiParameterDocMap, rightResponses: ApiParameterDocMap): ResponseDiff {
     const diffItems: ResponseDiff = {}
+    return diffItems
+}
+
+function compareModels(leftModel: ApiModelDoc, rightModel: ApiModelDoc, basePath: string): DiffItem[] {
+    const diffItems: DiffItem[] = []
     return diffItems
 }
