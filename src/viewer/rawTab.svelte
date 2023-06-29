1e1a772b1b9e0c3e3379de5944d248ef1387a1bf
mlucarella <Marco.LUCARELLA@amadeus.com>
2023-06-29 19:40:45 +0200

    Display raw json in codemirror

--- src/viewer/rawTab.svelte
+++ src/viewer/rawTab.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
     import { API_TO_VALIDATE_INDENTATION } from 'cli/cliConstants';
+    import { minimalSetup } from 'codemirror';
+    import { lineNumbers } from '@codemirror/view';
+    import { foldGutter } from '@codemirror/language';
+    import CodeMirror from 'svelte-codemirror-editor';
+    import { githubLight } from '@uiw/codemirror-theme-github';
+    import { json } from '@codemirror/lang-json';
 
     export let apiDoc: any = null;
+    let apiText = '';
+
+    const codeMirrorExtensions = [minimalSetup, lineNumbers(), foldGutter()];
+
+    $: apiText = JSON.stringify(apiDoc, null, API_TO_VALIDATE_INDENTATION);
 </script>
 
 <div class="block">
     <div class="field">
         <div class="control">
-            <textarea class="textarea" readonly rows="20">{JSON.stringify(apiDoc, null, API_TO_VALIDATE_INDENTATION)}</textarea>
+            <CodeMirror theme={githubLight} editable={true} readonly={true} value={apiText} lang={json()} basic={false} extensions={codeMirrorExtensions} />
         </div>
     </div>
 </div>
